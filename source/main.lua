66d88fbde9100c3ae1bfbe1a1164bac6d2114495
Rodrigo Gallegos Anda <91966757+gigosaurio@users.noreply.github.com>
2024-02-11 21:47:22 +0100

    testing

--- source/main.lua
+++ source/main.lua
@@ -1,12 +1,13 @@
 import "CoreLibs/graphics"
 
 local pd <const> = playdate
-local gfx <const> = playdate.graphics
+local gfx <const> = pd.graphics
 
 playerX, playerY = 200,120
 playerRadius = 10
 
 function playdate.update() 
     gfx.clear()
+    local crankAngle = pd.getCr
     gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
 end
